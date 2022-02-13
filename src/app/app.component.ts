fff6fd80dee0132f39c3b44e501d25f6acc1a056
Ajay Managaon <ajaymanagaon@gmail.com>
2022-02-13 10:56:54 +0530

    Committing Angular Code 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'Web.DatingApp.UI';
-  users : any;
+  users: any;
 
-  constructor(private http : HttpClient){
-
-  }
+  constructor(private http: HttpClient) {}
   ngOnInit() {
     this.getUsers();
   }
 
-  getUsers(){
-    this.http.get("http://localhost:5272/Users").subscribe(
-      response => {
+  getUsers() {
+    this.http.get('http://localhost:5272/Users').subscribe({
+      next: (response) => {
         this.users = response;
       },
-      error => {
+      error: (error) => {
         console.log(error);
-      }      
-    )
+      },
+    });
   }
 }
